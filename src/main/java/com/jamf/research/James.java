a78e5e9468c5e3eefb6ae49bf52bd0aee5a55bf4
Matt W Armstrong <mattwarmstrong@matts-mbp.wireless-dhcp.ec.jamfsw.corp>
2017-01-11 12:15:54 -0600

    Matt Says - Edit

--- src/main/java/com/jamf/research/James.java
+++ src/main/java/com/jamf/research/James.java
@@ -1,16 +1,16 @@
 package com.jamf.research;
 
 /**
- * Created by jamesfelton on 1/11/17.
+ * Created by jamesfelton on 1/11/17. Edited by Mattarm 1/11
  */
-public class James {
+public class Matt {
 
 	public static String saySomething() {
 		int selection = (int) (Math.random() * 1000);
-		String[] sayings =  {"Just say 'No' to plug-ins!",
-				"Serverless code, man! Serverless code.",
-				"Did someone say Lambda",
-				"Dungeons & Dragons!"};
+		String[] sayings =  {"Lambda and Kinesis",
+				"We need datastores",
+				"Science of analytics & Understanding data",
+				"Go Cowboys"};
 		return sayings[selection % sayings.length];
 	}
 }
--- src/main/java/com/jamf/research/JamfSays.java
+++ src/main/java/com/jamf/research/JamfSays.java
@@ -3,7 +3,7 @@ package com.jamf.research;
 public class JamfSays {
 
     public static void main(String[] args) {
-        James.saySomething();
+        Matt.saySomething();
     }
 
 }
